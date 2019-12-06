ed940bd8a58b5ff162b5c7122e48c23d270dfcd4
Andrew Schwenn <aschwenn@verizon.net>
2019-12-05 22:09:16 -0600

    states

--- watcher.py
+++ watcher.py
@@ -8,12 +8,14 @@ import subprocess
 GPIO.setmode(GPIO.BCM)
 
 ''' CONSTANTS '''
-TURNOFF_DELAY_SECONDS = 3
+TURNOFF_DELAY_SECONDS = 20
 SENSOR_PIN = 17
-USE_ALWAYS_ON_OVERRIDE = True
+USE_ALWAYS_ON_OVERRIDE = False
 ALWAYS_ON_PIN = 20
-USE_ALWAYS_OFF_OVERRIDE = True
+ALWAYS_ON_STATE = 0 # will enable upon reading this value
+USE_ALWAYS_OFF_OVERRIDE = False
 ALWAYS_OFF_PIN = 16
+ALWAYS_OFF_STATE = 0 # will enable upon reading this value
 
 ''' PROGRAM '''
 def main():
@@ -27,26 +29,24 @@ def main():
  
     while True:
         if USE_ALWAYS_ON_OVERRIDE:
-            if GPIO.input(ALWAYS_ON_PIN):
+            if GPIO.input(ALWAYS_ON_PIN) == ALWAYS_ON_STATE:
                 if turned_off:
                     turned_off = False
                     turn_on()
                 continue
         if USE_ALWAYS_OFF_OVERRIDE:
-            if GPIO.input(ALWAYS_OFF_PIN):
+            if GPIO.input(ALWAYS_OFF_PIN) == ALWAYS_OFF_STATE:
                 if not turned_off:
                     turned_off = True
                     turn_off()
                 continue
         if GPIO.input(SENSOR_PIN):
-            print('pin 17 on')
             last_motion_time = time.time()
             sys.stdout.flush()
             if turned_off:
                 turned_off = False
                 turn_on()
         else:
-            print('pin 17 off')
             if not turned_off and time.time() > (last_motion_time + TURNOFF_DELAY_SECONDS):
                 turned_off = True
                 turn_off()
